a84b55a414ae9ede93241433b0d8ed64a391d456
Mylo <36931363+gitmylo@users.noreply.github.com>
2022-06-25 14:49:40 +0200

    fix not calling actions

--- cs/menu.cs
+++ cs/menu.cs
@@ -35,6 +35,10 @@ namespace SimpleMenu
                     {
                         Console.WriteLine("\tInvalid choice");
                     }
+                    else
+                    {
+                        optionsNumbered[choice].action();
+                    }
                 }
                 else
                 {
